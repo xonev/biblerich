acfb364f490961ec4df8e091ececeaafda5c60e4
Steven Oxley <steven.aj.oxley@gmail.com>
2013-07-26 21:21:26 -0500

    Added the ten commandments page.
    Made some other tweaks.

--- scripts/convert_pages_to_html.rb
+++ scripts/convert_pages_to_html.rb
@@ -8,15 +8,33 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage convert_pages_to_html.rb filename"
 end.parse!
+
+if ARGV.length != 2
+  puts "Incorrect usage"
+  exit(1)
+end
+
 pn = Pathname.pwd + ARGV[0]
+title = ARGV[1]
+
+def yml_front(title)
+  puts """---
+layout: default
+title: Bible Rich | #{title}
+---"""
+end
+
+def xml_to_html(xml)
+  doc = Nokogiri::XML.parse(xml)
+  first_line = true
+  doc.search('//sf:p/text()').each do |p|
+    element = first_line ? 'h1' : 'p'
+    yield "<#{element}>#{p.content}</#{element}>"
+    first_line = false
+  end
+end
 
 data = pn.read
 gz = Zlib::GzipReader.new(StringIO.new(data))
-xml = gz.read
-doc = Nokogiri::XML.parse(xml)
-first_line = true
-doc.search('//sf:p/text()').each do |p|
-  element = first_line ? 'h1' : 'p'
-  puts "<#{element}>#{p.content}</#{element}>"
-  first_line = false
-end
+puts yml_front title
+xml_to_html gz.read, &method(:puts).to_proc
