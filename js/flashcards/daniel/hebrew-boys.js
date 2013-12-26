7b4aa485e10b8a144a70eaaaf8c633e7e4c8092b
Steven Oxley <steven.aj.oxley@gmail.com>
2013-12-26 17:21:23 -0600

    Remove names for Hebrew boys.

--- js/flashcards/daniel/hebrew-boys.js
+++ js/flashcards/daniel/hebrew-boys.js
@@ -2,20 +2,20 @@
 $(function () {
   return Flashcards.init('/bible-highlights/review/daniel/hebrew-boys.html', [
     {
-      front: '<img src="/img/flashcards/daniel/boys-daniel.png" alt="click to see other side"></img>\n              <h3>Daniel</h3>',
-      back: '<img src="/img/flashcards/daniel/boys-belteshazzar.png" alt="click to see other side"></img>\n             <h3>Belteshazzar</h3>'
+      front: '<img src="/img/flashcards/daniel/boys-daniel.png" alt="click to see other side"></img>',
+      back: '<img src="/img/flashcards/daniel/boys-belteshazzar.png" alt="click to see other side"></img>'
     },
     {
-      front: '<img src="/img/flashcards/daniel/boys-hananiah.png" alt="click to see other side"></img>\n              <h3>Hananiah</h3>',
-      back: '<img src="/img/flashcards/daniel/boys-shadrach.png" alt="click to see other side"></img>\n             <h3>Shadrach</h3>'
+      front: '<img src="/img/flashcards/daniel/boys-hananiah.png" alt="click to see other side"></img>',
+      back: '<img src="/img/flashcards/daniel/boys-shadrach.png" alt="click to see other side"></img>'
     },
     {
-      front: '<img src="/img/flashcards/daniel/boys-mishael.png" alt="click to see other side"></img>\n              <h3>Mishael</h3>',
-      back: '<img src="/img/flashcards/daniel/boys-meshach.png" alt="click to see other side"></img>\n             <h3>Meshach</h3>'
+      front: '<img src="/img/flashcards/daniel/boys-mishael.png" alt="click to see other side"></img>',
+      back: '<img src="/img/flashcards/daniel/boys-meshach.png" alt="click to see other side"></img>'
     },
     {
-      front: '<img src="/img/flashcards/daniel/boys-azariah.png" alt="click to see other side"></img>\n              <h3>Azariah</h3>',
-      back: '<img src="/img/flashcards/daniel/boys-abednego.png" alt="click to see other side"></img>\n             <h3>Abednego</h3>'
+      front: '<img src="/img/flashcards/daniel/boys-azariah.png" alt="click to see other side"></img>',
+      back: '<img src="/img/flashcards/daniel/boys-abednego.png" alt="click to see other side"></img>'
     }
   ]);
 });
